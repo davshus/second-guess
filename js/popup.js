87fdd378163ed231b0802992de9615176b510563
David Shustin <david.shustin@gmail.com>
2017-03-26 05:55:07 -0400

    Eat spaghetti

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,12 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	document.getElementById("portsubmit").addEventListener("click", function() {
-		var status = document.getElementById("status");
-		if (!status.firstChild) {
-			status.appendChild(document.createTextNode(""));
-		}
-		var portin = document.getElementById("portinput").value;
-		if (portin == undefined || portin == null || portin == "" || isNaN(portin)) {
-			status.firstChild.nodeValue = "Error: Port is not a number.";
-		} else {
-			status.firstChild.nodeValue = "Opening port...";
-		}
-	});
+document.getElementById("portsubmit").addEventListener("click", function() {
+	var status = document.getElementById("status");
+	if (!status.firstChild) {
+		status.appendChild(document.createTextNode(""));
+	}
+	var portin = document.getElementById("portinput").value;
+	if (portin == undefined || portin == null || portin == "" || isNaN(portin)) {
+		status.firstChild.nodeValue = "Error: Port is not a number.";
+	} else {
+		status.firstChild.nodeValue = "Opening port...";
+	}
 });
